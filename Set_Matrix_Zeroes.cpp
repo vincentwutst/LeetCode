204e53c3f4cdf530f18a77bc7a47f1aa0f27fb08
vincentwutst <vincent.wu.tst@gmail.com>
2015-02-10 16:43:29 +1100

    define two vectors to record row and col

---
+++ Set_Matrix_Zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+	void setZeroes(vector<vector<int> > &matrix) {
+		int m = matrix.size();
+		int n = matrix[0].size();
+		vector<int> row(m);//m个0
+		vector<int> col(n);//n个0
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				if (matrix[i][j] == 0)
+				{
+					row[i] = 1;
+					col[j] = 1;
+				}
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				if (row[i] == 1 || col[j] == 1)
+					matrix[i][j] = 0;
+	}
+};
