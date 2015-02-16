12bd856d91a07773f77199f3cf226661c7b56732
vincentwutst <vincent.wu.tst@gmail.com>
2015-02-16 17:42:55 +1100

    DFS

---
+++ Generate_Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	void dfs(vector<string> &ret, string &tmp, int left, int right, int n) {
+		if (left == n && right == n) {
+			ret.push_back(tmp);
+		}
+		if (left < n) {
+			tmp.append(1, '(');
+			dfs(ret, tmp, left + 1, right, n);
+			tmp.resize(tmp.size() - 1);
+		}
+		if (left > right) {
+			tmp.append(1, ')');
+			dfs(ret, tmp, left, right + 1, n);
+			tmp.resize(tmp.size() - 1);
+		}
+	}
+	vector<string> generateParenthesis(int n) {
+		vector<string> ret;
+		string tmp = "";
+		dfs(ret, tmp, 0, 0, n);
+		return ret;
+	}
+};
