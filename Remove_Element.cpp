b9760c74117c26e8a256f515456a6b0924289dda
vincentwutst <vincent.wu.tst@gmail.com>
2015-02-17 16:39:09 +1100

    array

---
+++ Remove_Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int removeElement(int A[], int n, int elem) {
+		int cnt = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (A[i] != elem)
+				A[cnt++] = A[i];
+		}
+		return cnt;
+
+	}
+};
