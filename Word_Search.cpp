53363f06698e6682dee7fcd59ce53d4089e14f18
vincentwutst <vincent.wu.tst@gmail.com>
2015-02-23 21:34:08 +1100

    DFS

---
+++ Word_Search.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+	bool exist(vector<vector<char> > &board, string word) {
+		const int m = board.size();//board 共m行，n列
+		const int n = board[0].size();
+		
+		//新建m*n的矩阵，标记该单元是否被访问过
+		vector<vector<bool> > visited(m, vector<bool>(n, false));
+		for (int i = 0; i < m; ++i)
+			for (int j = 0; j < n; ++j)
+				if (dfs(board, word, 0, i, j, visited))
+					return true;
+		return false;
+	}
+private:
+	static bool dfs(const vector<vector<char> > &board, const string &word,
+		int index, int x, int y, vector<vector<bool> > &visited) //深搜
+	{
+		if (index == word.size())
+			return true; // 收敛条件
+
+		if (x < 0 || y < 0 || x >= board.size() || y >= board[0].size())
+			return false; // 越界，终止条件
+
+		if (visited[x][y]) return false; // 已经访问过，剪枝
+
+		if (board[x][y] != word[index]) return false; // 不相等，剪枝
+
+		visited[x][y] = true;
+		bool ret = dfs(board, word, index + 1, x - 1, y, visited) || // 上
+			       dfs(board, word, index + 1, x + 1, y, visited) || // 下
+			       dfs(board, word, index + 1, x, y - 1, visited) || // 左
+			       dfs(board, word, index + 1, x, y + 1, visited);   // 右
+		visited[x][y] = false;
+		return ret;
+	}
+};
